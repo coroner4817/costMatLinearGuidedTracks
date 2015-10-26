6241456fb7d44a1c3fb4b308a04f132f381733e1
FabianPeters <petersfbn@googlemail.com>
2015-10-26 23:13:39 +0000

    added licence

--- costMatLinearGuidedTracks_closeGaps.m
+++ costMatLinearGuidedTracks_closeGaps.m
@@ -116,6 +116,17 @@ function [costMat,nonlinkMarker,indxMerge,numMerge,indxSplit,numSplit,errFlag] =
 %       numSplit      : Number of such tracks.
 %       errFlag       : 0 if function executes normally, 1 otherwise.
 %
+%
+% you can redistribute and/or modify this file under the terms of the GNU General Public License
+% as published by the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% It is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+
 
 %% initial checks
 
